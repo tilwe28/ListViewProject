ca149e5de813296b7bca02737abb2a70b56a76d2
Anurag Tilwe <anurag.tilwe6@gmail.com>
2020-12-14 13:36:21 -0500

    Added Landscape Orientation
    Added and fixed variable names and ids

--- app/src/main/java/com/example/listviewprojectqtwo/MainActivity.java
+++ app/src/main/java/com/example/listviewprojectqtwo/MainActivity.java
@@ -32,12 +32,12 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.main_listView);
 
         consoles = new ArrayList<>();
-        consoles.add(new Console(R.drawable.wii, "Wii"));
-        consoles.add(new Console(R.drawable.switchnintendo, "Nintendo Switch"));
-        consoles.add(new Console(R.drawable.xbox360, "Xbox 360"));
-        consoles.add(new Console(R.drawable.xboxone, "Xbox One"));
-        consoles.add(new Console(R.drawable.ps3, "PS3"));
-        consoles.add(new Console(R.drawable.ps4, "PS4"));
+        consoles.add(new Console(R.drawable.wii, "Wii", "November 19, 2006", 249.99));
+        consoles.add(new Console(R.drawable.switchnintendo, "Nintendo Switch", "March 3, 2017", 299.99));
+        consoles.add(new Console(R.drawable.xbox360, "Xbox 360", "November 22, 2005", 299.99));
+        consoles.add(new Console(R.drawable.xboxone, "Xbox One", "Novermber 22, 2013", 399.0));
+        consoles.add(new Console(R.drawable.ps3, "PS3", "November 11, 2006", 599.99));
+        consoles.add(new Console(R.drawable.ps4, "PS4", "November 15, 2013", 399.0));
 
         ListViewAdapter adapter = new ListViewAdapter(this, R.layout.adapter_listview, consoles);
         listView.setAdapter(adapter);
@@ -46,19 +46,30 @@ public class MainActivity extends AppCompatActivity {
 
     public class Console {
         int image;
-        String text;
+        String name, date;
+        Double price;
 
-        public Console (int image, String text) {
+        public Console (int image, String name, String date, Double price) {
             this.image = image;
-            this.text = text;
+            this.name = name;
+            this.date = date;
+            this.price = price;
         }
 
         public int getImage() {
             return image;
         }
 
-        public String getText() {
-            return text;
+        public String getName() {
+            return name;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public Double getPrice() {
+            return price;
         }
     }
 
@@ -80,11 +91,11 @@ public class MainActivity extends AppCompatActivity {
             LayoutInflater inflater = (LayoutInflater)myContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             View adapterView= inflater.inflate(xml,null);
             ImageView imageView = adapterView.findViewById(R.id.adapter_imageView);
-            TextView textView = adapterView.findViewById(R.id.adapter_textView);
+            TextView textView = adapterView.findViewById(R.id.adapter_textView_name);
             Button button = adapterView.findViewById(R.id.adapter_button);
 
             imageView.setImageResource(listy.get(position).getImage());
-            textView.setText(listy.get(position).getText());
+            textView.setText(listy.get(position).getName());
             button.setText("DELETE");
 
             return adapterView;
